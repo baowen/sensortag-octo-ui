198d0b78408034294b1ef4d5251b0870af361495
Matthew Carpenter <matthew.carpenter@dvla.gov.uk>
2019-03-19 14:43:22 +0000

    First look at connecting to the webservice, react hooks and socket.io

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000');
+
+function App() {
+  const [connected, setConnection] = useState(false);
+
+  useEffect(() => {
+    if (connected) {
+      console.log('joining socket');
+      socket.emit('test-send', 'PING!');
+    }
+
+    return () => {
+      if (connected) {
+        console.log('leaving socket');
+        socket.emit('test-send', 'MOO');
+      }
+    };
+  });
+
+  function displayConnectedMessage() {
+    const connectedMessage = 'You have connected to the socket';
+    if (connected) {
+      return connectedMessage;
+    }
   }
+
+  const handleConnection = () => {
+    connected ? setConnection(false) : setConnection(true);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          {displayConnectedMessage()}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={() => handleConnection()}>
+          {connected && `End Connection`}
+          {!connected && `Start Connection`}
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
